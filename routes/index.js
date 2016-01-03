f13b3887d9123ca3c041f51ca78be46ce4f68267
James Xue <jamesxue100@gmail.com>
2016-01-02 16:41:37 -0800

    prereqs working, restructured node app

---
+++ routes/index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/classes';
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Classes' });
+});
+
+router.post('/', function(req, res){
+    course = req.body.course;
+
+    MongoClient.connect(url, function(err, db) {
+      console.log(course);
+      assert.equal(null, err);
+      findPrereqs(db, course, function(response) {
+          courses = response[0];
+          actions = response[1]; 
+          console.log(courses);
+          console.log(actions);
+          db.close();
+          res.status(200).json({success:true, content:[courses, actions]}).end();
+      });
+      // console.log(courses);
+      
+    });   
+});
+
+var findPrereqs = function(db, course, callback) {
+    course_list = []
+    action_list = []
+    var cursor =db.collection('classes').find({ "courses.course": course } );
+    cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+         for (i = 0; i < doc.courses.length; i++){
+          if (doc.courses[i].course == course){
+            prereqs = doc.courses[i].prerequisites;
+            for (j = 0; j < prereqs.length; j++){
+              courses = prereqs[j].courses;
+              action = prereqs[j].action_needed;
+              course_list.push(courses);
+              action_list.push(action);
+            }
+          }         
+        }
+        return callback([course_list, action_list]);
+      } 
+    });
+};
+
+module.exports = router;
+
